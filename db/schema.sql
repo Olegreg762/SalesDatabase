4a753e0d09d6ab3f04d732d4c26313d56dda2172
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2026-03-27 12:12:06 -0500

    Add initial database schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS sales_db;
+CREATE DATABASE sales_db;
+USE sales_db;
+
+CREATE TABLE Sales (
+    salesperson_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    department_number INT NOT NULL,
+    customers INT NOT NULL,
+    zip_code VARCHAR(10) NOT NULL,
+    annual_sales DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE SalesRegions (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    region VARCHAR(50) NOT NULL,
+    customers INT NOT NULL,
+    customer_territory VARCHAR(50) NOT NULL,
+    region_sales DECIMAL(10, 2) NOT NULL,
+    sales_person_id INT NOT NULL,
+    FOREIGN KEY (sales_person_id) REFERENCES Sales(salesperson_id)
+);
