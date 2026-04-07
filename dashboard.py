e4b8294e0c718d28cbc7b24041de44614292586f
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2026-04-07 13:58:13 -0500

    Refactor dashboard.py to improve region selection and update README.md with dashboard query details

--- dashboard.py
+++ dashboard.py
@@ -27,7 +27,7 @@ over25k = st.secrets["over_25k"]
 sales_query = st.secrets["sales_query"]
 report_sale = st.secrets["report_sale"]
 report_region = st.secrets["report_region"]
-region_options = [" "] + run_query(st.secrets["region_options"])["region"].tolist()
+region_options = run_query(st.secrets["region_options"])["region"].tolist()
 query_options = ["Sales Over $25k", "Region Sales Query", "Sales Person Report", "Region Report"]
 
 if KEY not in st.session_state:
@@ -44,7 +44,7 @@ def nav_change():
 
 
 st.title("Sales Dashboard")
-selection = st.segmented_control(
+selection = st.pills(
     label="Navigation",
     options=query_options,
     key=KEY,
@@ -57,18 +57,31 @@ if selection == "Sales Over $25k":
     st.subheader("Sales Over $25k")
     df = run_query(over25k)
     st.dataframe(df, width="content")
+
 elif selection == "Region Sales Query":
+    def clear_selection():
+        st.session_state["region_selection"] = []
     st.subheader("Region Sales Query")
-    selected_region = st.selectbox("Filter by Region", options=region_options)
-    if selected_region != " ":
-        df = run_query(sales_query + f" WHERE region = '{selected_region}'")
+    selected_region = st.pills(
+        "Filter by Region", 
+        options=region_options, 
+        selection_mode="multi",
+        key="region_selection",)
+    st.button("Clear Selection", 
+              on_click=lambda:st.session_state.update({"region_selection": []}))
+    if len(selected_region) > 1:
+        df = run_query(sales_query + f" WHERE region IN {tuple(selected_region)}")
+    elif len(selected_region) == 1:
+        df = run_query(sales_query + f" WHERE region = '{selected_region[0]}'")
     else:
         df = run_query(sales_query)
     st.dataframe(df, width="content")
+
 elif selection == "Sales Person Report":
     st.subheader("Sales Person Report")
     df = run_query(report_sale)
     st.dataframe(df, width="content")
+    
 elif selection == "Region Report":
     st.subheader("Region Report")
     df = run_query(report_region)
